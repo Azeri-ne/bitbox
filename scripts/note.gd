ffb481f001c1ab976d20b370277bc4691b050ced
Azeri-ne <loudenhal@protonmail.com>
2025-11-18 20:49:49 +0800

    Initial commit

---
+++ scripts/note.gd
@@ -0,0 +1,6 @@
+class_name Note
+extends Node2D
+
+var info: HitObject
+
+@onready var color_rect: ColorRect = %ColorRect
---
+++ scripts/timing_point.gd
@@ -0,0 +1,8 @@
+## Resource representing a point in time that tracks the tempo of audio.
+class_name TimingPoint
+extends Resource
+
+## Start time.
+@export var time: float
+## Beats per minute; tempo.
+@export var bpm: float
