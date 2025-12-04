ac5546d5641ea2423ffd14cae0ae177d9a358b50
Azeri-ne <loudenhal@protonmail.com>
2025-12-04 17:10:54 +0800

    Add Talo player authentication

--- scripts/account_creation.gd
+++ scripts/account_creation.gd
@@ -1,10 +1,10 @@
 extends Control
 
-@onready var username_input: TextEdit = $UsernameInput
-@onready var email_input: TextEdit = $EmailInput
-@onready var password_input: TextEdit = $PasswordInput
+@onready var username_input: LineEdit = $UsernameInput
+@onready var email_input: LineEdit = $EmailInput
+@onready var password_input: LineEdit = $PasswordInput
 
-@onready var account_creation_status: Label = $Status
+@onready var status: Label = $Status
 
 @onready var create_account_button: Button = $CreateAccount
 @onready var back_button: Button = $GotoMain
@@ -15,7 +15,29 @@ func init_back_button():
 	)
 	
 func init_account_creation_status():
-	account_creation_status.hide()
+	status.hide()
+
+func create_account():
+	var res = await Talo.player_auth.register(
+		username_input.text,
+		password_input.text,
+	)
+	
+	if res == OK:
+		status.text = "Success!"
+		status.show()
+
+	if res != OK:
+		match Talo.player_auth.last_error.get_code():
+			TaloAuthError.ErrorCode.IDENTIFIER_TAKEN:
+				status.text = "Username is taken"
+			
+			_:
+				status.text = Talo.player_auth.last_error.get_string()
+		status.show()
 
 func _ready() -> void:
 	init_back_button()
+	init_account_creation_status()
+	
+	create_account_button.pressed.connect(create_account)
