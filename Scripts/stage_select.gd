0ea8e9557979a914b2a917053a45f3df595f30d0
Azeri-ne <loudenhal@protonmail.com>
2025-12-09 15:19:47 +0800

    Shit

--- Scripts/stage_select.gd
+++ Scripts/stage_select.gd
@@ -2,6 +2,14 @@ extends Control
 
 @onready var Music = preload("res://Assets/P3P - Title screen theme.mp3")
 
+@onready var session_total_score: Label = $SessionTotalScore
+
+func init_score():
+	session_total_score.text = str(0)
+
+func _ready():
+	init_score()
+
 func _on_stage_1_pressed() -> void:
 	#SceneLoader.change_scene("res://Scenes/stage.tscn")
 	Transition.go_to("res://Scenes/stage.tscn")
