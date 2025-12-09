66ec4995f229590b3f9fa60a78b48ddbe0008178
Azeri-ne <loudenhal@protonmail.com>
2025-12-09 13:50:15 +0800

    Remove unused assets

--- scripts/note.gd
+++
@@ -1,6 +0,0 @@
-class_name Note
-extends Node2D
-
-var info: HitObject
-
-@onready var color_rect: ColorRect = %ColorRect
--- scripts/timing_point.gd
+++
@@ -1,8 +0,0 @@
-## Resource representing a point in time that tracks the tempo of audio.
-class_name TimingPoint
-extends Resource
-
-## Start time.
-@export var time: float
-## Beats per minute; tempo.
-@export var bpm: float
